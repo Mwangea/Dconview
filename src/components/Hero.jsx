a0d325d7f2584d38e8cc30af56cb9f11307f7676
mwangea <mwangeamusa@gmail.com>
2024-01-11 12:31:26 +0300

    modified the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+//import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div 
+     id="Home"
+     className="w-[100%] h-[120vh]"
+     style={{
+      backgroundImage: `url("/hero.jpg")`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center center",
+     }}
+    >
+    <div className="h-[100%] w-[100%] bg-black/50 flex flex-col-reverse  justify-start md:gap-24 gap-8 items-center">
+        <div className="w-[90%] mx-auto flex flex-col bg-[#22485e] justify-center items-center sm:rounded-t-lg md:rounded-t-xl lg:rounded-t-2xl">
+          <div className="p-8 grid md:grid-cols-4">
+            <div className="flex gap-1 items-start">
+              <img src="/vector.png" 
+                className="w-[28px] object-contain h-[34px]"
+              />
+              <div className="flex text-white flex-col gap-1">
+                <p className="font-bold text-xl">
+                  Community Empowerment
+                </p>
+                <p className="text-xs">
+                  To foster economic growth and provide opportunities
+                  within the communities we serve.
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-1 items-start">
+              <img src="/vector.png" 
+                className="w-[28px] object-contain h-[34px]"
+              />
+              <div className="flex text-white flex-col gap-1">
+                <p className="font-bold text-xl">
+                  Community Empowerment
+                </p>
+                <p className="text-xs">
+                  To foster economic growth and provide opportunities
+                  within the communities we serve.
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-1 items-start">
+              <img src="/vector.png" 
+                className="w-[28px] object-contain h-[34px]"
+              />
+              <div className="flex text-white flex-col gap-1">
+                <p className="font-bold text-xl">
+                  Community Empowerment
+                </p>
+                <p className="text-xs">
+                  To foster economic growth and provide opportunities
+                  within the communities we serve.
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-1 items-start">
+              <img src="/vector.png" 
+                className="w-[28px] object-contain h-[34px]"
+              />
+              <div className="flex text-white flex-col gap-1">
+                <p className="font-bold text-xl">
+                  Community Empowerment
+                </p>
+                <p className="text-xs">
+                  To foster economic growth and provide opportunities
+                  within the communities we serve.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=""></div>
+    </div>
+
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,13 @@ import { AiFillMail } from "react-icons/ai";
 import { IoIosCall } from "react-icons/io";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => { 
+  const [nav, setNav] = useState(false);
+  const toggleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className='w-[100%] z-10  bg-[#22485e]'>
       <div className="md:w-[80%] mx-auto md:flex hidden justify-between">
@@ -28,7 +33,7 @@ const Navbar = () => {
          </div>
       </div>
       <p className="bg-[rgb(146,122,122)] h-[1px] w-[100%]" />
-       <div className="py-4 md:w-[80%] w-[95%]  flex justify-between items-center mx-auto">
+       <div className="py-6 my-1 md:w-[80%] w-[95%]  flex justify-between items-center mx-auto space-x-7">
         <Link 
         to="/"
         className="flex md:gap-2 items-center font-semibold text-2xl">
@@ -60,7 +65,48 @@ const Navbar = () => {
           >
           Contact
           </Link>
+          <button className="bg-[#FF0000] rounded-md text-white p-2">
+            Get Started
+          </button>
         </div>
+        <AiOutlineMenu className="text-4xl md:hidden" onClick={toggleNav} />
+       </div>
+       <div 
+         className={
+          nav ? "sm:hidden absolute top-0 right-0 left-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-black text-center duration-300 ease-in-out"
+          : "sm:hidden absolute top-0 right-0 left-[100%] bottom -0 flex justify-center items-center w-full h-screen bg-white text-black  text-center duration-300 ease-in-out"
+         }
+       >
+        <p className="absolute top-4 right-4">
+          <AiOutlineClose className="text-4xl" onClick={toggleNav} />
+        </p>
+        <ul onClick={toggleNav} className="flex flex-col text-2xl gap-8">
+        <Link to="/"
+          className="hover:text-[#FF0000] transition-all ease-in-out duration-500 cursor-pointer"
+          >
+          Home
+          </Link>
+          <Link to="/About"
+          className="hover:text-[#FF0000] transition-all ease-in-out duration-500 cursor-pointer"
+          >
+          About
+          </Link>
+          <Link to="/Services"
+          className="hover:text-[#FF0000] transition-all ease-in-out duration-500 cursor-pointer"
+          >
+          Services
+          </Link>
+          <Link to="/Team"
+          className="hover:text-[#FF0000] transition-all ease-in-out duration-500 cursor-pointer"
+          >
+          Team
+          </Link>
+          <Link to="/Contact"
+          className="hover:text-[#FF0000] transition-all ease-in-out duration-500 cursor-pointer"
+          >
+          Contact
+          </Link>
+        </ul>
        </div>
     </div>
   )
